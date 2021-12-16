0b0889d5b5786f38f3460c8fb6aaecac158c2573
azorko <asskarra@gmail.com>
2021-12-16 23:53:59 +0300

    task_1

---
+++ islands_task.py
@@ -0,0 +1,45 @@
+islands = [
+        ['1', '1', '1', '0', '0'],
+        ['1', '1', '1', '0', '0'],
+        ['1', '1', '1', '0', '0'],
+        ['1', '1', '0', '0', '1'],
+        ['1', '0', '0', '0', '1'],
+    ]
+
+islands2 = [
+    ['0', '0', '0', '1', '0', '0', '0'],
+    ['0', '0', '1', '1', '0', '0', '0'],
+    ['0', '1', '1', '0', '0', '1', '0'],
+    ['1', '1', '1', '1', '1', '1', '0'],
+    ['0', '0', '1', '1', '1', '0', '0'],
+]
+
+
+def check_points(arr, i, j):
+    if i + 1 in range(len(arr)) and arr[i + 1][j] == '1':
+        arr[i + 1][j] = 't'
+        check_points(arr, i + 1, j)
+    if i - 1 in range(len(arr)) and arr[i - 1][j] == '1':
+        arr[i - 1][j] = 't'
+        check_points(arr, i - 1, j)
+    if j + 1 in range(len(arr[0])) and arr[i][j + 1] == '1':
+        arr[i][j + 1] = 't'
+        check_points(arr, i, j + 1)
+    if j - 1 in range(len(arr[0])) and arr[i][j - 1] == '1':
+        arr[i][j - 1] = 't'
+        check_points(arr, i, j - 1)
+
+
+def islands_counter(arr):
+    count = 0
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if arr[i][j] == '1':
+                count += 1
+                arr[i][j] = 't'
+                check_points(arr, i, j)
+    return count
+
+
+print(islands_counter(islands))
+print(islands_counter(islands2))
